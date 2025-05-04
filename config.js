de1e576ecd883ff5cb71775b85aba4cacf7375e7
Amaan <150792171+md-amaan-sid@users.noreply.github.com>
2025-05-04 18:31:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeG54a0pl_CCSv7lEeVt2-7FIY_YImbLU",
+  authDomain: "netflix-clone-7ccc7.firebaseapp.com",
+  projectId: "netflix-clone-7ccc7",
+  storageBucket: "netflix-clone-7ccc7.firebasestorage.app",
+  messagingSenderId: "197929387552",
+  appId: "1:197929387552:web:1f0c0a54e0b0b24bad1e45",
+};
+
+// craete a/c on themoviedb.org and replace access token below
+
+const TMDB_Access_Key =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjk4M2ZiNDJiN2RhMWY5YmExOWY5ZDM3NGQ3ZDg3YSIsIm5iZiI6MTc0MzYxMzE0OS45OTgwMDAxLCJzdWIiOiI2N2VkNmNkZDA2MDcyMDcwNzBjZTY0NzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.To8cWMsKiilbQ9kdeev7RaJfpoVfOBUQb2_BgnnVIc8";
+
+export { firebaseConfig, TMDB_Access_Key };
+
